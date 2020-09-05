e6f698efa242028d4286fb61b30335893cd0b7d5
mk <midaslmg94@gmail.com>
2020-09-06 03:27:15 +0900

    react 기초 day 01 - React 기본 세팅 및 컴포넌트 학습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Person } from './Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Person name={'이민기'} age={27}></Person>
+        <Person name={'홍길동'} age={27}></Person>
+        <Person name={'아악'} age={47}></Person>
+      </div>
+    );
+  }
 }
-
 export default App;
